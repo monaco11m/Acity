036e216b0e4752d533d9473ed57b671ae2e9eff6
monaco11m <elmer.chp@gmail.com>
2025-12-18 09:42:01 -0500

    minor update

--- auth-service/Auth.Application/Interfaces/IRefreshTokenRepository.cs
+++ auth-service/Auth.Application/Interfaces/IRefreshTokenRepository.cs
@@ -9,6 +9,7 @@ namespace Auth.Application.Interfaces
 {
     public interface IRefreshTokenRepository
     {
+
         Task<RefreshToken?> GetByTokenAsync(string token);
         Task AddAsync(RefreshToken refreshToken);
         Task InvalidateAsync(RefreshToken refreshToken);
